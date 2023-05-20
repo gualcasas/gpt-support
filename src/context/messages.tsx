b83eba971ad9b649693f86c56b3db7f2e9f212b7
Gual Casas <gual.casas@gmail.com>
2023-05-20 13:49:01 -0600

    setup messages context

--- src/context/messages.tsx
+++ src/context/messages.tsx
@@ -1,3 +1,82 @@
-import { createContext } from "react";
+import type { Message } from "@/lib/validators/message";
+import { nanoid } from "nanoid";
+import { createContext, useCallback, useState } from "react";
 
-export const MessagesContext = createContext<>({})
+type TMessagesContext = {
+    messages: Message[];
+    isMessageUpdating: boolean;
+    addMessage: (message: Message) => void;
+    removeMessage: (id: string) => void;
+    updateMessage: (id: string, updateFn: (prevText: string) => string) => void;
+    setIsMessageUpdating: (isUpdating: boolean) => void;
+};
+
+export const MessagesContext = createContext<TMessagesContext>({
+    messages: [],
+    isMessageUpdating: false,
+    addMessage: () => null,
+    removeMessage: () => null,
+    updateMessage: () => null,
+    setIsMessageUpdating: () => null,
+});
+
+export const MessagesContextProvider = ({
+    children,
+}: {
+    children: React.ReactNode;
+}) => {
+    const [isMessageUpdating, setIsMessageUpdating] = useState<boolean>(false);
+    const [messages, setMessages] = useState<TMessagesContext["messages"]>([
+        {
+            id: nanoid(),
+            isUserMessage: false,
+            text: "Hello, how can I help you?",
+        },
+    ]);
+
+    const addMessage: TMessagesContext["addMessage"] = useCallback(
+        (message) => {
+            setMessages((prev) => [...prev, message]);
+        },
+        []
+    );
+
+    const removeMessage: TMessagesContext["removeMessage"] = useCallback(
+        (id) => {
+            setMessages((prev) => prev.filter((msg) => msg.id !== id));
+        },
+        []
+    );
+
+    const updateMessage: TMessagesContext["updateMessage"] = useCallback(
+        (id, updateFn) => {
+            setMessages((prev) =>
+                prev.map((message) => {
+                    if (message.id === id) {
+                        return {
+                            ...message,
+                            text: updateFn(message.text),
+                        };
+                    }
+                    return message;
+                })
+            );
+        },
+        []
+    );
+
+    return (
+        <MessagesContext.Provider
+            value={{
+                messages,
+                isMessageUpdating,
+                addMessage,
+                removeMessage,
+                updateMessage,
+                setIsMessageUpdating,
+            }}
+        >
+            {children}
+        </MessagesContext.Provider>
+    );
+};
